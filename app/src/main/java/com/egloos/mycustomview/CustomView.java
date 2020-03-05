11e23fb57bf4c299febe3867dc37466ba89d1c9a
leeyunhome <leeyunhome@daum.net>
2020-03-05 09:08:13 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/mycustomview/CustomView.java
@@ -0,0 +1,51 @@
+package com.egloos.mycustomview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class CustomView extends View {
+    Paint paint;
+
+    public CustomView(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public void init(Context context){
+        paint = new Paint();
+        paint.setColor(Color.RED);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //canvas.drawRect(100, 100, 200, 200, paint);
+        
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            Toast.makeText(getContext(), "눌렸음 : " + event.getX() + ", "
+            + event.getY(), Toast.LENGTH_LONG).show();
+        }
+
+        return super.onTouchEvent(event);
+    }
+}
